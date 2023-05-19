d09d3740c97f00fcdfbbc31ae5f715331ddd2da0
Kinle <1435351453@qq.com>
2023-05-19 23:17:34 +0800

    蓝桥杯13届国赛

---
+++ 蓝桥杯/13届国赛/01数组划分/js/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Object} oldArr
+ * @param {Object} num
+ * */
+const splitArray = (oldArr, num) => {
+  // TODO：请补充代码实现功能
+  var arr = oldArr.sort();
+  const len = oldArr.length;
+
+  if (len <= num) {
+    return arr;
+  }
+  const pnum = Math.ceil(len / num)
+  // console.log(len);
+  // console.log(num);
+  // console.log(pnum);
+  let res = []
+  for (let i = 0; i < pnum; i++) {
+    let r1 = [];
+
+    for (let j = i * num; j < i * num + num && j < len; j++) {
+      r1.push(arr[j]);
+    }
+    res.push(r1)
+  }
+  return res
+};
+module.exports = splitArray; // 检测需要，请勿删除
---
+++ 蓝桥杯/13届国赛/04简易计算器/js/index.js
@@ -0,0 +1,57 @@
+// TODO：请补充代码
+
+const $ = selector => document.querySelector(selector)
+
+// console.log(new Function('a', 'b', 'return a+b')(1, 50))
+const formula = $('#formula')
+const result = $('#result')
+formula.value = ''
+let calc = ''
+result.value = ''
+let hasPoint = false
+const addSymbol = (innerText, sym) => {
+  hasPoint = false
+  formula.value += innerText
+  calc += sym
+}
+
+for (const btn of document.querySelectorAll('.calc-button')) {
+  btn.onclick = () => {
+    formula.value += btn.innerText
+    calc += btn.innerText
+  }
+}
+
+$('#add').onclick = e => addSymbol(e.target.innerText, '+')
+$('#reduce').onclick = e => addSymbol(e.target.innerText, '-')
+$('#multiply').onclick = e => addSymbol(e.target.innerText, '*')
+$('#division').onclick = e => addSymbol(e.target.innerText, '/')
+$('#sqrt').onclick = e => {
+  console.log(Math.sqrt(eval(calc)))
+  let res = Math.sqrt(eval(calc))
+  result.value = Number.isInteger(res) ? res : 'NaN'
+}
+$('#decimal').onclick = e => {
+  if (hasPoint) {
+    return
+  }
+
+  hasPoint = true
+  formula.value += e.target.innerText
+  calc += '.'
+}
+$('#equal').onclick = () => {
+  console.log(calc)
+  let res = eval(calc)
+  result.value = res
+  // 简单处理一下溢出
+  if ((res + '').indexOf('.') > -1 && (res + '').split('.')[1].length > 8) {
+    result.value = res.toFixed(2)
+  }
+}
+$('#reset').onclick = e => {
+  hasPoint = false
+  formula.value = ''
+  calc = ''
+  result.value = ''
+}
